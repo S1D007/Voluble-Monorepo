765b316c5b276f8b2753245ddd9fbe90f7770d31
Siddhant <s1d.official011001011@gmail.com>
2024-06-09 12:09:32 +0530

    chore: initial project setup

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    singleQuote: true,
+    trailingComma: 'all',
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    bracketSameLine: true,
+    printWidth: 120,
+    useTabs: false,
+    tabWidth: 2,
+    endOfLine: 'lf',
+    jsxBracketSameLine: false,
+    jsxSingleQuote: false,
+    htmlWhitespaceSensitivity: 'strict',
+  };
